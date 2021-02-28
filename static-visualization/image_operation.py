b86a7b6bf6633e8850f762f0d07e98fab5751af5
tolga <tolga.reis@trapmine.com>
2021-02-28 09:33:49 +0300

    count_vector method that creates byte frequency vector created.

--- static-visualization/image_operation.py
+++ static-visualization/image_operation.py
@@ -1,16 +1,29 @@
 import os 
 
-def read_bin(filename):
-    """Reads the binary file that is given as parameter and converts it to list. 
+def decimal_vector(filename):
+    """Reads the binary file that is given as parameter and converts it to decimal integer list. 
 
     Args:
         filename (string): The name of the file that is read.
     """
 
-    byte_list = list()
+    decimal_vector = list()
     with open(filename, 'rb') as binary_file:
         binary_data = binary_file.read(1)
         while binary_data != b'':
-            byte_list.append(ord(binary_data))
+            decimal_vector.append(ord(binary_data))
             binary_data = binary_file.read(1)
-    return byte_list
+    return decimal_vector
+
+def count_vector(decimal_vector):
+    """Constructs a count vector based on the occurrence of numbers in the decimal vector (byte frequency)
+
+    Args:
+        decimal_vector ([integer]): 8-bit unsigned integers read from the binary.
+
+    Returns:
+        [integer]: Byte frequency vector.
+    """
+    count_vector = [decimal_vector.count(byte) for byte in range(0, 256)]
+
+    return count_vector
