4050babb168633142e1193e6e8b0b6ebb3823f0a
tolga <tolga.reis@trapmine.com>
2021-02-28 09:48:16 +0300

    math_operation method that reduces the influence of possible outliers.

---
+++ static-visualization/math_operation.py
@@ -0,0 +1,13 @@
+import math
+
+def atan_norm(x):
+    """The arctan function grows more and more slowly as its input increases, 
+       so it is applied in hope of reducing the influence of possible outliers.
+
+    Args:
+        x (int): Byte frequency of a byte integer between 0-255.
+
+    Returns:
+        float: arctan norm of the x value.
+    """
+    return (math.atan(x) * 2 / math.pi) + 1
