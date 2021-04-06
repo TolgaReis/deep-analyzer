a4c3d55ad18539624dcbc48fa8a8cda6ba92798d
tolga <tolgaareis@gmail.com>
2021-04-06 12:04:18 +0300

    Slow visualization solved.

--- static-visualization/image_operation.py
+++ static-visualization/image_operation.py
@@ -1,35 +1,25 @@
 import math_operation
 import numpy as np
+from config import Config
 
-def create_decimal_vector(filename):
+def create_vectors(filename):
     """Reads the binary file that is given as parameter and
        converts it to decimal integer list.
 
     Args:
         filename (string): The name of the file that is read.
     """
-
-    vector = list()
+    decimal_vector = list()
+    count_vector = [0] * (Config.MAX_UNSIGNED_BYTE_VAL + 1)
     with open(filename, 'rb') as binary_file:
         binary_data = binary_file.read(1)
         while binary_data != b'':
-            vector.append(ord(binary_data))
+            decimal_data = ord(binary_data)
+            decimal_vector.append(decimal_data)
+            count_vector[decimal_data] += 1
             binary_data = binary_file.read(1)
-    return vector
 
-def create_count_vector(vector):
-    """Constructs a count vector based on the occurrence of
-       numbers in the decimal vector (byte frequency)
-
-    Args:
-        decimal_vector ([int]): 8-bit unsigned integers read
-        from the binary.
-
-    Returns:
-        [int]: Byte frequency vector.
-    """
-
-    return [vector.count(byte) for byte in range(0, 256)]
+    return decimal_vector, count_vector
 
 def create_pixel_vector(count_vector, decimal_vector):
     """Takes count and decimal vectors, then returns the pixel vector.
