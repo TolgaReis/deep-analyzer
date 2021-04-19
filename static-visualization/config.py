104f82fa5e42fe6136b069124817c9c98bddaf77
tolga <tolgaareis@gmail.com>
2021-04-19 10:27:46 +0300

    Paths in config.py updated.

--- static-visualization/config.py
+++ static-visualization/config.py
@@ -4,11 +4,11 @@ class Config:
     MAX_UNSIGNED_BYTE_VAL = 255
 
     DATA_TYPES = ['malware', 'benign']
-    DATA_DIR = 'data/%s/unloaded/%s.jpeg'
+    DATA_DIR = '../data/%s/unloaded/%s.jpeg'
 
-    FILES_BENIGN_UNCONVERTED = 'files/benign/unconverted'
-    FILES_BENIGN_CONVERTED = 'files/benign/converted'
+    FILES_BENIGN_UNCONVERTED = '../files/benign/unconverted'
+    FILES_BENIGN_CONVERTED = '../files/benign/converted'
 
-    FILES_MALWARE_UNCONVERTED = 'files/malware/unconverted'
-    FILES_MALWARE_CONVERTED = 'files/malware/converted'
+    FILES_MALWARE_UNCONVERTED = '../files/malware/unconverted'
+    FILES_MALWARE_CONVERTED = '../files/malware/converted'
     
